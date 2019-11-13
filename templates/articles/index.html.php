b9cef8769e514f5831210fd6a4c38b293d776cb2
edenolam <julienbasquin.jb@gmail.com>
2019-11-14 00:48:45 +0100

    design

--- templates/articles/index.html.php
+++ templates/articles/index.html.php
@@ -1,9 +1,14 @@
 <h1>Nos articles</h1>
 
 <?php foreach ($articles as $article) : ?>
-    <h2><?= $article['title'] ?></h2>
-    <small>Ecrit le <?= $article['created_at'] ?></small>
-    <p><?= $article['introduction'] ?></p>
-    <a href="article.php?id=<?= $article['id'] ?>">Lire la suite</a>
-    <a href="delete-article.php?id=<?= $article['id'] ?>" onclick="return window.confirm(`Êtes vous sur de vouloir supprimer cet article ?!`)">Supprimer</a>
+<div class="card">
+   <div class="card-body">
+       <h4 class="card-title"><?= $article['title'] ?></h4>
+       <small>Ecrit le <?= $article['created_at'] ?></small>
+       <p class="card-text"><?= $article['introduction'] ?></p>
+
+    <a href="article.php?id=<?= $article['id'] ?>" class="btn btn-warning">Lire la suite</a>
+    <a href="delete-article.php?id=<?= $article['id'] ?>" onclick="return window.confirm(`Êtes vous sur de vouloir supprimer cet article ?!`)" class="btn btn-danger">Supprimer</a>
+   </div>
+</div>
 <?php endforeach ?>
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -10,9 +10,29 @@
 </head>
 
 <body>
-    <div class="container">
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container">
+      <a class="navbar-brand" href="#"><?= $pageTitle ?></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="/">tout les articles</a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</nav>
+<div class="container">
         <?= $pageContent ?>
+</div>
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <p>Footer</p>
     </div>
+
 </body>
 
 </html>
